59c8646b249533b5a13f20ddc909f539a895843a
JeongWu-Jane <park96100331@gmail.com>
2023-12-03 14:37:09 +0900

    froontend api test

--- my-project/frontend/src/App.tsx
+++ my-project/frontend/src/App.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get("/find/1");
+        setUser(response.data);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{user ? <p>api 테스트 성공</p> : <p>api 테스트 실패</p>}</p>
       </header>
     </div>
   );

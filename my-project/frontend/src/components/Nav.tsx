4ac06cc3bb2fa4d5351d59e4dd37147c95c1439b
JeongWu-Jane <park96100331@gmail.com>
2024-01-02 22:23:26 +0900

    make Navbar

--- my-project/frontend/src/components/Nav.tsx
+++ my-project/frontend/src/components/Nav.tsx
@@ -1,37 +1,49 @@
 import "../styles/Nav.css";
 import { Link } from "react-router-dom";
+import { ReactComponent as HouseIcon } from "../assets/icons/house.svg";
+import { ReactComponent as MenuIcon } from "../assets/icons/menu.svg";
+import { ReactComponent as SearchIcon } from "../assets/icons/search.svg";
+import { ReactComponent as MyIcon } from "../assets/icons/my.svg";
 
 function Nav() {
   return (
     <nav className="nav">
-      <ul>
-        <li>
+      <div className="nav-list">
+        <div className="nav-list-item">
           <Link to="/">
-            <div className="img"></div>
+            <div className="img">
+              <HouseIcon />
+            </div>
             <div className="title">HOME</div>
           </Link>
-        </li>
-        <li>
+        </div>
+        <div className="nav-list-item">
           <Link to="/">
             {" "}
-            <div className="img"></div>
+            <div className="img">
+              <MenuIcon />
+            </div>
             <div className="title">CATEGORY</div>
           </Link>
-        </li>
-        <li>
+        </div>
+        <div className="nav-list-item">
           <Link to="/">
-            <div className="img"></div>
+            <div className="img">
+              <SearchIcon />
+            </div>
             <div className="title">SEARCH</div>
           </Link>
-        </li>
-        <li>
+        </div>
+        <div className="nav-list-item">
           <Link to="/">
             {" "}
-            <div className="img"></div>
+            <div className="img">
+              <MyIcon />
+            </div>
             <div className="title">MY</div>
           </Link>
-        </li>
-      </ul>
+        </div>
+      </div>
     </nav>
   );
 }
